1c13804b6b2515f84445998e127aab4710c3875b
Mark Ciril <markciril.jamon@gmail.com>
2026-01-06 11:07:07 +0800

    style: improve the design of the selected theme and make the base content dynamic

--- src/components/ModeToggle.vue
+++ src/components/ModeToggle.vue
@@ -2,21 +2,20 @@
     <DropdownMenu>
         <DropdownMenuTrigger as-child>
             <Button variant="outline">
-                <Icon icon="radix-icons:moon"
-                    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Icon icon="radix-icons:sun"
-                    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span class="sr-only">Toggle theme</span>
+                <Icon :icon="selectedTheme(mode)" class="h-[1.2rem] w-[1.2rem]" />
             </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-            <DropdownMenuItem @click="mode = 'light'">
+            <DropdownMenuItem @click="mode = 'light'" class="flex gap-2" :class="{ 'bg-accent': mode === 'light' }">
+                <Icon icon="radix-icons:sun" />
                 Light
             </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'dark'">
+            <DropdownMenuItem @click="mode = 'dark'" class="flex gap-2" :class="{ 'bg-accent': mode === 'dark' }">
+                <Icon icon="radix-icons:moon" />
                 Dark
             </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'auto'">
+            <DropdownMenuItem @click="mode = 'auto'" class="flex gap-2" :class="{ 'bg-accent': mode === 'auto' }">
+                <Icon icon="material-symbols-light:computer-outline" />
                 System
             </DropdownMenuItem>
         </DropdownMenuContent>
@@ -28,6 +27,16 @@ import { Icon } from '@iconify/vue'
 import { useColorMode } from '@vueuse/core'
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+
+function selectedTheme(theme: string) {
+    if (theme === 'light') {
+        return 'radix-icons:sun'
+    } else if (theme === 'dark') {
+        return 'radix-icons:moon'
+    } else {
+        return 'material-symbols-light:computer-outline'
+    }
+}
 
 const mode = useColorMode()
 </script>
