f087ac1b308e515793ca72ff92592a8f2f0f6734
Mark Ciril <markciril.jamon@gmail.com>
2026-01-07 14:57:34 +0800

    feat: create configuration for vitest

---
+++ vitest.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./test/setup.ts'],
+    include: ['test/**/*.{test,spec}.{js,ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        'test/**',
+        '**/server/**',
+        'src/test/**'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
